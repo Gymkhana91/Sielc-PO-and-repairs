c708991e468cb69b2ccebba7ddbc57c94043fc33
Alex <ereminpro@gmail.com>
2025-07-15 16:46:07 -0500

    Добавил создание суперпользователя в build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ pip install -r requirements.txt
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+echo "Creating superuser..."
+python manage.py shell < create_superuser.py
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
