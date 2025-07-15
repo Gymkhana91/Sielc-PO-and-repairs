e6a6bdc3f851fb198e0cd89bdccb22e205c0c4e5
Alex <ereminpro@gmail.com>
2025-07-15 16:57:21 -0500

    Удалил create_superuser.py после успешного создания

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ pip install -r requirements.txt
 echo "Running migrations..."
 python manage.py migrate --noinput
 
-echo "Creating superuser..."
-python manage.py shell < create_superuser.py
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
