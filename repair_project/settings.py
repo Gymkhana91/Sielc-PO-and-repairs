9acb36f98bcb976442ce811a21a4bad68bb09a7d
Alex <ereminpro@gmail.com>
2025-07-15 12:09:16 -0500

    Настроил статику для админки

--- repair_project/settings.py
+++ repair_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +77,13 @@ WSGI_APPLICATION = 'repair_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://instruments_db_user:c5T42YFHDmNwNXqR5Wy3bGjqdLwfJjFE@dpg-d1r5tkjuibrs73f6jp4g-a/instruments_db',
+        conn_max_age=600
+    )
 }
+
+
 
 
 # Password validation
@@ -116,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
