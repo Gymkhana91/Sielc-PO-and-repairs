a25beba14c5d4c1e560e65942fd919028cc581a3
Alex <ereminpro@gmail.com>
2025-07-16 12:26:48 -0500

    Удалён импорт несуществующей модели RepairAndService

--- repairs/admin.py
+++ repairs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, RepairAndService  # Оставим только нужное
+from .models import Order, Device
 
 admin.site.register(Order)
-admin.site.register(RepairAndService)
+
