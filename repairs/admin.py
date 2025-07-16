afc76db6defca2cfda18a39401c1e26c53220d9b
Alex <ereminpro@gmail.com>
2025-07-16 12:23:02 -0500

    Удалены ненужные зависимости от ConfigurationOption

--- repairs/admin.py
+++ repairs/admin.py
@@ -1,19 +1,5 @@
 from django.contrib import admin
-from .models import Device, ConfigurationType, ConfigurationOption, Order
+from .models import Order, RepairAndService  # Оставим только нужное
 
-class ConfigurationOptionInline(admin.TabularInline):
-    model = ConfigurationOption
-    extra = 1
-
-class ConfigurationTypeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'device']
-    inlines = [ConfigurationOptionInline]
-
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['order_date', 'device', 'customer_name']
-    filter_horizontal = ('configurations',)  # чтобы удобно выбирать много конфигураций
-
-admin.site.register(Device)
-admin.site.register(ConfigurationType, ConfigurationTypeAdmin)
-admin.site.register(ConfigurationOption)
-admin.site.register(Order, OrderAdmin)
+admin.site.register(Order)
+admin.site.register(RepairAndService)
--- repairs/models.py
+++ repairs/models.py
@@ -6,26 +6,8 @@ class Device(models.Model):
     def __str__(self):
         return self.name
 
-
-class ConfigurationType(models.Model):
-    device = models.ForeignKey(Device, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.device.name} – {self.name}"
-
-
-class ConfigurationOption(models.Model):
-    config_type = models.ForeignKey(ConfigurationType, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"{self.config_type.name}: {self.name}"
-
-
 class Order(models.Model):
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
-    configurations = models.ManyToManyField(ConfigurationOption)
     customer_name = models.CharField(max_length=100)
     order_date = models.DateField()
 
